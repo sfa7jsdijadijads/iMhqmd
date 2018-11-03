ecdbb71077431fd86557c6aa6b3ce2433517955f
jkdksjgkjkdfgdfgdfgsdg <43796038+jkdksjgkjkdfgdfgdfgsdg@users.noreply.github.com>
2018-11-03 16:41:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,25 +23,12 @@ client.on('message', message => {
     }
     });
 
-client.on('ready',async () => {
-  let GUILDID = '497502071218503721';
-  let CHANNELID = '501092028306685973'; 
-  voiceStay(GUILDID, CHANNELID);
-  function voiceStay(guildid, channelid) {
-    if(!guildid) throw new Error('Syntax: voiceStay function requires guildid');
-    if(!channelid) throw new Error('Syntax: voiceStay function requires channelid');
-
-    let guild = client.guilds.get(guildid);
-    let channel = guild.channels.get(channelid);
-
-    if(channel.type === 'voice') {
-      channel.join().catch(e => {
-        console.log(`Failed To Join :: ${e.message}`);
-      });
-    } else {
-      console.log(`Channel Type :: ${channel.type}, It must be Voice.`);
+client.on('message', message => {
+    if(message.content.startsWith('!say')) {
+    if(message.author.id !== "302536427235704832") return;
+    var args = message.content.split(' ').slice(1).join(' ');
+    message.channel.send(args);
     }
-  }
-});
+    });
 
 client.login(process.env.BOT_TOKEN);
