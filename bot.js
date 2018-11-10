9d8ba263f77d916e80014b660d6272b39447263f
sfa7jsdijadijads <44931403+sfa7jsdijadijads@users.noreply.github.com>
2018-11-10 23:39:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,41 +2,23 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
- console.log("welcome bot"); 
-console.log("log");
+  console.log(`Logged in as ${client.user.tag}!`);  
 });
 
 client.on('ready', async() => {
-var server = "497499938448605194"; 
-var channel = "498890133450391574";
+var server = "510915761963794433";
+var channel = "510915761963794439";
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('yousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxcoyousif-jxco123456789')
+    client.guilds.get(server).channels.get(channel).send('**S-nkoh , S-nkoh , S-nkoh , S-nkoh , S-nkoh , S-nkoh , S-nkoh , S-nkoh , S-nkoh , S-nkoh , S-nkoh , S-nkoh , S-nkoh , S-nkoh , S-nkoh , S-nkoh , S-nkoh , S-nkoh , **')
     },305);
 })
 
-
 client.on('message', message => {
     if(message.content.startsWith('!say')) {
-    if(message.author.id !== "501376527251013661") return;
+    if(message.author.id !== "449498327655120896") return;
     var args = message.content.split(' ').slice(1).join(' ');
     message.channel.send(args);
     }
     });
-
-client.on('message', message => {
-    if(message.content.startsWith('!say')) {
-    if(message.author.id !== "302536427235704832") return;
-    var args = message.content.split(' ').slice(1).join(' ');
-    message.channel.send(args);
-    }
-    });
-
-client.on('message', message => {
-    if(message.content.startsWith('!say')) {
-    if(message.author.id !== "349046898562170883") return;
-    var args = message.content.split(' ').slice(1).join(' ');
-    message.channel.send(args);
-    }
-    });
-
+ 
 client.login(process.env.BOT_TOKEN);
